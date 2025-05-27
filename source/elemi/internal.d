0ad9db43aa33d240cc161e13552ec4b53e37c910
Artha <artha@samerion.com>
2025-05-27 23:42:19 +0200

    Close #30: implement shorthand attribute syntax

--- source/elemi/internal.d
+++ source/elemi/internal.d
@@ -9,9 +9,18 @@ pure @safe:
 static if (__traits(compiles, { import core.interpolation; })) {
     public import core.interpolation;
     enum withInterpolation = true;
+    
+    enum isInterpolation(size_t index, Ts...)
+        =  is(Ts[index] == InterpolationHeader)
+        || is(Ts[index] == InterpolationFooter)
+        || is(Ts[index] == InterpolatedLiteral!text, string text)
+        || is(Ts[index] == InterpolatedExpression!text, string text)
+        || (index > 0 && is(Ts[index-1] == InterpolatedExpression!text, string text));
+
 }
 else {
     enum withInterpolation = false;
+    enum isInterpolation(size_t index, Ts...) = false;
 }
 
 /// Escape HTML elements.
